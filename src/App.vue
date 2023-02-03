76a459cc28146ddced6bee82f9de83afd1f40478
koxa17 <koxa17@mail.ru>
2023-02-03 12:41:39 +0500

    title and component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,25 @@
 <template>
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Title test</h1>
+  <h1>Title test</h1>
+  <h1>Title test</h1>
+  <h1>Title test</h1>
+  <h1>Title test</h1>
+  <h1>Title test</h1>
+  <h1>Title test</h1>
   <test-comp />
+  <servise-list/>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 import TestComp from "@/components/TestComp";
+import ServiseList from "@/components/ServiseList";
 
 export default {
   name: 'App',
   components: {
+    ServiseList,
     TestComp,
     HelloWorld
   }
